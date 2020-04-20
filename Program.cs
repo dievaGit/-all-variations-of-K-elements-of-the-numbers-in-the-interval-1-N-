2914c95f329c2a5e9a5bd8ce82c0545127c5d225
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-20 13:15:12 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, k = 0;
+            
+
+            Console.WriteLine(" Write the number of interval to the arragement:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Write the number of variations to the arragement:");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            int[] interval = new int[k];
+            int contInterval = 0;
+
+            while(contInterval < n)
+            {
+                for (int i = 1; i <= k; i++)
+                {
+
+                }
+            }
+            
+
+
+        }
+    }
+}
